cc5a75898d6798275b5cc540a2308e8d5f06ed51
LAPTOP-21O86I06\LZM <1104407395@qq.com>
2025-12-30 21:02:55 +0800

    初步设置界面，重新制作头框，并增加按钮事件

--- LearnChatRoom/LearnChatRoom/MainWindow.xaml.cs
+++ LearnChatRoom/LearnChatRoom/MainWindow.xaml.cs
@@ -20,5 +20,49 @@ namespace LearnChatRoom
         {
             InitializeComponent();
         }
+
+        #region 界面按钮事件
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.IsActive)
+                {
+                    window.WindowState = WindowState.Minimized;
+                }
+            }
+        }
+
+        private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.IsActive)
+                {
+                    if (window.WindowState != WindowState.Maximized)
+                    {
+                        window.WindowState = WindowState.Maximized;
+                    }
+                    else
+                    {
+                        window.WindowState = WindowState.Normal;
+                    }
+                }
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
     }
 }
