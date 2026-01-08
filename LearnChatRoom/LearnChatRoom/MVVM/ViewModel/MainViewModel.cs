bda91584dc1a1ba42014b1507974ec57c785f122
LAPTOP-21O86I06\LZM <1104407395@qq.com>
2026-01-08 14:47:55 +0800

    将右框绑定到聊天界面(选择对应选项会显示对应对话),增加了滑动条的资源

--- LearnChatRoom/LearnChatRoom/MVVM/ViewModel/MainViewModel.cs
+++ LearnChatRoom/LearnChatRoom/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using LearnChatRoom.MVVM.Model;
+﻿using LearnChatRoom.Core;
+using LearnChatRoom.MVVM.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,15 +9,57 @@ using System.Threading.Tasks;
 
 namespace LearnChatRoom.MVVM.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /* Commands */
+
+        public RelayCommand SendCommand { get; set; }
+
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set
+            {
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+
 
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+
+                Message = "";
+            });
 
             Messages.Add(new MessageModel
             {
